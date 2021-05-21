87fec6ded1e57df525c879a23ca460a63f108e24
dsouza.rogerio <dsouza.rogerio@gmail.com>
2021-05-21 18:31:06 -0300

    Commit Inicial

---
+++ src/main/java/com/meuprojeto/soulog/SoulogApiApplication.java
@@ -0,0 +1,13 @@
+package com.meuprojeto.soulog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulogApiApplication.class, args);
+	}
+
+}
